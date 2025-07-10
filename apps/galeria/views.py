4d28ed7ede3bb3ed682810a267936e34095243a4
João Vitor Carvalho Barbosa <joaovitorcarvalho903@gmail.com>
2025-07-10 15:11:09 -0300

    feat(gallery): add image upload form with validation and file support

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 def index(request):
@@ -31,7 +32,20 @@ def buscar(request):
     return render(request, "galeria/buscar.html", {"cards": fotografias})
 
 def nova_imagem(request):
-    return render(request, 'galeria/nova_imagem.html')
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não está logado.")
+        return redirect('login')
+
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada')
+            return redirect('index')
+
+
+    return render(request, 'galeria/nova_imagem.html', {"form": form})
 
 
 def editar_imagem(request):
